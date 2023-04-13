c746c1498c64e0b570bb8a5f0b6bf36dce8ab423
NikitkaKoev <nikita-koev@mail.ru>
2023-04-13 10:34:12 +0300

    2 commit

---
+++ ЛК Сотрудника/scripts.js
@@ -0,0 +1,8 @@
+function togglePlaque() {
+    var plaque = document.getElementById("plaque");
+    if (plaque.style.display === "none") {
+      plaque.style.display = "block";
+    } else {
+      plaque.style.display = "none";
+    }
+  }
