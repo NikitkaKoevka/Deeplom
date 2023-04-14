9e8c3f83aa481005870d763624be35afcd6d1076
NikitkaKoev <nikita-koev@mail.ru>
2023-04-15 02:03:55 +0300

    завершена работа фронта перед созданием бэка

--- ЛК Сотрудника/scripts.js
+++ ЛК Сотрудника/scripts.js
@@ -1,8 +1,18 @@
+var flag=1;
 function togglePlaque() {
     var plaque = document.getElementById("plaque");
-    if (plaque.style.display === "none") {
+    if (plaque.style.display === "none"|| flag==1 ) {
+      console.log("1");
       plaque.style.display = "block";
-    } else {
+      flag=0;
+    } else if( flag==0) {
+      console.log("2");
       plaque.style.display = "none";
     }
+  }
+
+
+  function SupportAlert()
+  {
+    alert("Контакты поддержки сайта:\nТелефон:+7-495-183-54-99\ne-mail: k3support@gmail.com");
   }
